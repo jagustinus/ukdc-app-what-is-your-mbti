4f6dd8ef9342c3216d4de7a76c6950d8b3337a84
ekaadev <ekaadev9@gmail.com>
2025-05-15 22:39:54 +0700

    feat(navbar): add navigation component with links to other page

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<nav class="flex h-16 items-center justify-start gap-3 bg-gray-600 text-zinc-100">
+	<a
+		href="/recommend-fields"
+		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/recommend-fields'
+			? 'border-orange-500 hover:border-b-orange-500'
+			: 'border-transparent hover:border-b-gray-900'} "
+	>
+		Recommend Fields</a
+	>
+	<a
+		href="/personality-test"
+		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/personality-test'
+			? 'border-orange-500 hover:border-b-orange-500'
+			: 'border-transparent hover:border-b-gray-900'} ">Personality Test</a
+	>
+</nav>
